bcada884c80c0f5a12155e3c13d44fe6e9bf6405
Maksim Sheleh <maximsheleh@gmail.com>
2022-09-29 11:23:07 +0300

    Fix getting cursor position on 4k monitor

--- lib/utils.py
+++ lib/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import keyboard
-import mouse
+import pyautogui
 import time
 
 
@@ -12,7 +12,7 @@ def get_rgb_from_numpy(np_array: np.ndarray, cord_x: int, cord_y: int):
 def get_absolute_cursor_position(key: str):
     while True:
         if keyboard.is_pressed(key):
-            x, y = mouse.get_position()
+            x, y = pyautogui.position()
             return x, y
         time.sleep(0.1)  # delay to not DDoS
 
@@ -22,11 +22,10 @@ def drag_and_get_box(key: str):
 
     while True:
         if keyboard.is_pressed(key):
-            x1, y1 = mouse.get_position()
-            print(x1, y1)
+            x1, y1 = pyautogui.position()
             while True:
                 if not keyboard.is_pressed(key):
-                    x2, y2 = mouse.get_position()
+                    x2, y2 = pyautogui.position()
                     return y1, x1, x2 - x1, y2 - y1
                 time.sleep(0.1)  # delay to not DDoS
         time.sleep(0.1)  # delay to not DDoS
